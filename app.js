0eb02f6da288a1d35e9fe9c928a56066fde3f63a
Percy Tomicha <mspercywillans@gmail.com>
2022-04-18 12:41:19 -0400

    Express v3 - Servidor de Contenido Estático

--- app.js
+++ app.js
@@ -3,16 +3,23 @@ const app = express()
 
 const port = 3000;
 
+// Servir contenido Estático
+app.use( express.static('public') );
+
+// Ignorará esta ruta, porque existe un index.html en la carpeta public/
 app.get('/', (req, res) => {
     res.send('Página de Inicio')
 });
 
+// Muestra esta ruta ya que en la carpeta public/ no existe un hola-mundo/index.html 
 app.get('/hola-mundo', (req, res) => {
     res.send('Hola Mundo')
 });
 
+// Muestrar esta ruta pero...
 app.get('*', (req, res) => {
-    res.send('404 | Page not Found')
+    // Indicando que archivo .html debe mostrar
+    res.sendFile( __dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
