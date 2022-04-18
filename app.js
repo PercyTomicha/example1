549c51c9d2fe28e14d51aa5b6440ff35377e149a
Percy Tomicha <mspercywillans@gmail.com>
2022-04-18 12:20:55 -0400

    Express v2 - Constante Puerto

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express()
 
+const port = 3000;
+
 app.get('/', (req, res) => {
     res.send('Página de Inicio')
 });
@@ -13,4 +15,6 @@ app.get('*', (req, res) => {
     res.send('404 | Page not Found')
 });
 
-app.listen(3000);
+app.listen(port, () => {
+    console.log(`Aplicación de Ejemplo escuchando en el http://localhost:${ port }`)
+});
