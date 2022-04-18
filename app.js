03c6a1ba3115575dace041087fc9b0b79f42b7fc
Percy Tomicha <mspercywillans@gmail.com>
2022-04-18 12:16:16 -0400

    Express v2 - Con Funciones Flechas

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const app = express()
 
-app.get('/', function (req, res){
+app.get('/', (req, res) => {
     res.send('Página de Inicio')
 });
 
-app.get('/hola-mundo', function (req, res){
+app.get('/hola-mundo', (req, res) => {
     res.send('Hola Mundo')
 });
 
-app.get('*', function (req, res){
+app.get('*', (req, res) => {
     res.send('404 | Page not Found')
 });
 
